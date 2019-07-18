a47e21088f821e102c426a2ef1f8df8c3fbe8566
André <algc321@gmail.com>
2019-07-18 17:19:36 -0300

    fourier 1 terminado

--- sketch.js
+++ sketch.js
@@ -1,17 +1,48 @@
-
 let tempo = 0;
+let onda = [];
+
+let slider;
 
 function setup(){
     createCanvas(600,400);
+    slider = createSlider(1, 50, 1)
 }
 
 function draw(){
     background(0);
-    translate(200,200);
-    let raio = 100;
-    stroke(255);
-    noFill();
-    ellipse(0,0,raio*2);
+    translate(150,200);
+    let x = 0;
+    let y = 0;
 
-    tempo += 0.01;
+    for (let i = 0; i<slider.value(); i++){
+        let prevx = x;
+        let prevy = y;
+        let n = (i*2)+1;
+        let raio = 75*(4/(n*PI));
+        stroke(255, 100);
+        noFill();
+        ellipse(prevx,prevy,raio*2);
+        x += raio*cos(n*tempo);
+        y += raio*sin(n*tempo);
+        
+        //fill(255);
+        stroke(255);
+        line(prevx,prevy,x,y);
+
+        //ellipse(x,y,8);
+}
+    onda.unshift(y);     
+    translate(200,0);
+    line(x-200,y,0,onda[0])   
+    beginShape();
+    noFill();
+    for(let i=0; i< onda.length; i++){
+        vertex(i,onda[i]);
+    }
+    endShape();
+    tempo += 0.03;
+    if (onda.length > 500){
+        onda.pop();
+    }
+
 }
