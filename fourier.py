ad4fe95d854a9cab249b7cc7c22b790ddf880b12
André <algc321@gmail.com>
2019-07-18 15:26:01 -0300

    fixed bugs

--- fourier.py
+++ fourier.py
@@ -9,9 +9,10 @@ screen = pygame.display.set_mode((800,800))
 
 while True:
     # Controle Quit
-    for envnt in pygame.event.get():
-        if event.type == pygame.QUIT():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
 
-            
+    screen.fill((255,255,255))
+    pygame.display.flip()
